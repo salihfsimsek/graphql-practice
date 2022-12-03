bacb4abe278daa19b1c10ee1abfa405b823142ad
salihsimseek@gmail.com <salihsimseek@gmail.com>
2022-12-04 01:16:10 +0300

    fragment added for event query

--- client/src/queries/Event.js
+++ client/src/queries/Event.js
@@ -1,25 +1,30 @@
 import { gql } from '@apollo/client'
 
+const eventsFragment = gql`
+  fragment EventsFragment on Event {
+    id
+    title
+    desc
+    date
+  }
+`
+
 export const GET_EVENTS = gql`
     query GetEvents{
-        events{
-            id
-            title
-            desc
-            date
-        }
+      events{
+          ...EventsFragment
+      }
     }
+    ${eventsFragment}
 `
 
 export const EVENTS_SUBSCRIPTION = gql`
   subscription{
     eventCreated{
-      id
-      title
-      desc
-      date
+      ...EventsFragment
     }
   }
+  ${eventsFragment}
 `
 
 export const GET_EVENT = gql`
