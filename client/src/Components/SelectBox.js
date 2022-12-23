8e50e8d5f48da22f8827611a57ca98797354ec39
salihsimseek@gmail.com <salihsimseek@gmail.com>
2022-12-23 23:01:48 +0300

    select box component created

---
+++ client/src/Components/SelectBox.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const SelectBox = ({ placeholder, value, changeValue, required, datas }) => {
+    return (
+        <select
+            value={value}
+            onChange={e => changeValue(e.target.value)}
+            required={required}
+            placeholder={placeholder}
+            className='bg-white border-2 border-gray-500 rounded-md p-2 w-full max-w-[500px] outline-none focus:border-gray-700'
+        >
+            <option value=''>{placeholder}</option>
+            {datas?.map((data, index) => (
+                <option key={index} value={data.id}>{data.name}</option>
+            )
+            )}
+        </select>
+    )
+}
+
+export default SelectBox
